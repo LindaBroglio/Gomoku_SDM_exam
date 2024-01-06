c0c760d7d4bfb4d7796d9ef67d828c238e2db524
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-06 12:36:14 +0100

    Add ColorTest

---
+++ src/test/java/GomokuTest/ColorTest.java
@@ -0,0 +1,14 @@
+package GomokuTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ColorTest {
+    @Test
+    public void testColorValues() {
+        assertEquals(0, Color.EMPTY.ordinal());
+        assertEquals(1, Color.BLACK.ordinal());
+        assertEquals(2, Color.WHITE.ordinal());
+    }
+}
+
