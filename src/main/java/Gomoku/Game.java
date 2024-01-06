a3fb843cd6ca0fd1ffbe500eb31e63ae23684853
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-06 18:17:06 +0100

    added possibility to input moves from terminal

--- src/main/java/Gomoku/Game.java
+++ src/main/java/Gomoku/Game.java
@@ -1,11 +1,15 @@
 package Gomoku;
 
+import java.util.Scanner;
 public class Game {
     private final Board board;
+    private final Scanner scanner;
+    private Integer turn = 0;
 
     // Constructor
     public Game(int boardSize) {
         this.board = new Board(boardSize);
+        this.scanner = new Scanner(System.in);
     }
 
     public Board getBoard() {
@@ -13,6 +17,26 @@ public class Game {
     }
 
     // Method to start the game
+    public void startGame() {
+        while (true) {
+            turn += 1;
+            this.board.displayBoard();
+            System.out.println("Enter your move (x y): ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Game ended.");
+                break;
+            }
+            int x = scanner.nextInt();
+            if (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Game ended.");
+                break;
+            }
+            int y = scanner.nextInt();
+            if (turn % 2 == 0) this.board.placeStone(x - 1, y - 1, Color.WHITE);
+            if (turn % 2 == 1) this.board.placeStone(x - 1, y - 1, Color.BLACK);
+        }
+    }
+
     // Method to end the game
     // Method to switch turns
     // ...
