3121b5e77d61d566d9eaf0b50c49c6a5db7a361a
LindaBroglio <LINDA.BROGLIO@studenti.units.it>
2024-01-06 15:50:45 +0100

    node pt1

--- src/main/java/Gomoku/Node.java
+++ src/main/java/Gomoku/Node.java
@@ -2,14 +2,31 @@ package Gomoku;
 
 public class Node {
     private Color color;
-    private int x_coordinate;
-    private int y_coordinate;
+    private int x;
+    private int y;
+    private Node east_neighbour;
+    private Node south_neighbour;
+    private Node south_east_neighbour;
 
     // Constructor
-    public Node(int x_coordinate, int y_coordinate) {
+    public Node(int x, int y) {
         this.color = Color.EMPTY;
-        this.x_coordinate = x_coordinate;
-        this.y_coordinate = y_coordinate;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void set_color(Color color) {
+        this.color = color;
+    }
+
+    public boolean has_same_east_neighbour(){
+        return this.color == east_neighbour.color;
+    }
+    public boolean has_same_south_neighbour(){
+        return this.color == south_neighbour.color;
+    }
+    public boolean has_same_south_east_neighbour(){
+        return this.color == south_east_neighbour.color;
     }
 
     // Getter and Setter methods for color, x_coordinate, and y_coordinate
