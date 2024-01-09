10698321f10c3279520cb995724b2f4a3a8fc0ed
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-09 17:34:22 +0100

    Diagonal check is now working:)

--- src/test/java/GomokuTest/BoardTest.java
+++ src/test/java/GomokuTest/BoardTest.java
@@ -77,26 +77,107 @@ public class BoardTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4})
-    void checkIfVerticalCheckWorksWithFullFirstRow(int index) throws TakenNodeException, OutOfBoardException {
+    @ValueSource(ints = {0, 1, 2})
+    void checkIfVerticalCheckWorks(int index) throws TakenNodeException, OutOfBoardException {
         Board board = new Board(5, 3);
         board.placeStone(0, 0, Color.BLACK);
         board.placeStone(1, 0, Color.BLACK);
         board.placeStone(2, 0, Color.BLACK);
-        board.placeStone(3, 0, Color.BLACK);
+        board.placeStone(3, 0, Color.WHITE);
         board.placeStone(4, 0, Color.BLACK);
         Assertions.assertTrue(board.enoughConsecutive(index, 0));
     }
 
-    @Disabled
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void checkIfDiagonalCheckWorksWhenFullyInsideTheBoard(int index) throws TakenNodeException, OutOfBoardException {
+        Board board = new Board(5, 3);
+        board.placeStone(1, 1, Color.BLACK);
+        board.placeStone(2, 2, Color.BLACK);
+        board.placeStone(3, 3, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(index, index));
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2})
-    void checkIfFirstDiagonalCheckWorksWithFullFirstRow(int index) throws TakenNodeException, OutOfBoardException {
+    void checkIfDiagonalCheckWorksWhenCloseToTheEdge(int index) throws TakenNodeException, OutOfBoardException {
         Board board = new Board(5, 3);
-        board.placeStone(1, 0, Color.BLACK);
+        board.placeStone(0, 0, Color.BLACK);
+        board.placeStone(1, 1, Color.BLACK);
+        board.placeStone(2, 2, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(index, index));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3, 4})
+    void checkIfOffDiagonalCheckWorks(int index) throws TakenNodeException, OutOfBoardException {
+        Board board = new Board(5, 3);
+        board.placeStone(4, 0, Color.BLACK);
+        board.placeStone(3, 1, Color.BLACK);
+        board.placeStone(2, 2, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(4 - index, index));
+    }
+
+    @Test
+    void checkeredPatternTest() throws TakenNodeException, OutOfBoardException {
+        Board board = new Board(5, 3);
+        for (int i = 0; i < board.boardSize(); i++)
+            for (int j = 0; j < board.boardSize(); j++)
+                if ((i + j) % 2 == 0) board.placeStone(i, j, Color.WHITE);
+                else board.placeStone(i, j, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(0,0));
+        Assertions.assertTrue(board.enoughConsecutive(0,1));
+        Assertions.assertTrue(board.enoughConsecutive(0,3));
+        Assertions.assertTrue(board.enoughConsecutive(0,4));
+        Assertions.assertTrue(board.enoughConsecutive(4,0));
+        Assertions.assertTrue(board.enoughConsecutive(4,1));
+        Assertions.assertTrue(board.enoughConsecutive(4,3));
+        Assertions.assertTrue(board.enoughConsecutive(4,4));
+        Assertions.assertTrue(board.enoughConsecutive(2,2));
+
+
+
+
+    }
+
+    @Test
+    void emptySquareFullSquareTest() throws TakenNodeException, OutOfBoardException {
+        Board board = new Board(5, 3);
+        // first row
+        board.placeStone(1, 1, Color.BLACK);
+        board.placeStone(1, 2, Color.BLACK);
+        board.placeStone(1, 3, Color.BLACK);
+        // second row
         board.placeStone(2, 1, Color.BLACK);
+        // center is empty
+        board.placeStone(2, 3, Color.BLACK);
+        // third row
+        board.placeStone(3, 1, Color.BLACK);
         board.placeStone(3, 2, Color.BLACK);
-        Assertions.assertTrue(board.enoughConsecutive(index +1, index));
+        board.placeStone(3, 3, Color.BLACK);
+        Assertions.assertFalse(board.enoughConsecutive(2,2));
+        // fill center
+        board.placeStone(2, 2, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(2,2));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    void checkIfMultipleWinningSequencesWork(int index) throws TakenNodeException, OutOfBoardException {
+        Board board = new Board(5, 3);
+        // Horizontal winning sequence
+        board.placeStone(0, 0, Color.BLACK);
+        board.placeStone(0, 1, Color.BLACK);
+        board.placeStone(0, 2, Color.BLACK);
+        // Vertical winning sequence
+        board.placeStone(0, 4, Color.BLACK);
+        board.placeStone(1, 4, Color.BLACK);
+        board.placeStone(2, 4, Color.BLACK);
+        // Diagonal winning sequence
+        board.placeStone(2, 0, Color.BLACK);
+        board.placeStone(3, 1, Color.BLACK);
+        board.placeStone(4, 2, Color.BLACK);
+        Assertions.assertTrue(board.enoughConsecutive(index, index));
     }
 
     @Test
