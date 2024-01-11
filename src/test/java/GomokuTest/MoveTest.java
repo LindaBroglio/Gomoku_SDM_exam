d10752fa76ae149353a088510dbf38eae6a9a9f4
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-11 23:34:02 +0100

    creation of the InputTest class and relative tests, they pass

--- src/test/java/GomokuTest/MoveTest.java
+++ src/test/java/GomokuTest/MoveTest.java
@@ -1,62 +1,42 @@
 package GomokuTest;
 
-import Gomoku.Board;
-import Gomoku.Exceptions.InputExceptions.QuitGameException;
+import Gomoku.Exceptions.InputExceptions.OutOfBoardException;
 import Gomoku.Game;
-import Gomoku.utilities.InputValidator;
 import Gomoku.utilities.Move;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.*;
 
-class MoveTest {
+public class MoveTest {
 
-    void setFakeStdInput(String fakeInput) {
-        ByteArrayInputStream fakeStandardInput = new ByteArrayInputStream(fakeInput.getBytes());
-        System.setIn(fakeStandardInput);
-    }
+    private Move move;
+    private Game game;
 
-    @BeforeEach
+    @Test
     @Disabled
-    void setUp() {
-        String inputData = "5\n3\n";
-        InputStream in = new ByteArrayInputStream(inputData.getBytes());
-        System.setIn(in);
-
-        //game = new Game();
-        //board = game.getBoard();
+    public void testMove_ValidMove() {
+        assertDoesNotThrow(() -> {
+            move.promptNextTurn(1);
+            move.readMove(1);
+            move.makeMove(1);
+        });
     }
 
     @Test
     @Disabled
-    void isQuitGameExceptionThrownIfInputIsQuit() {
-        //setFakeStdInput("quit" + System.lineSeparator());
-        //InputValidator inputValidator = new InputValidator(1);
-        //assertThrows(QuitGameException.class, inputValidator.validateInput("quit"));
+    public void testMove_OutOfBounds() {
+        assertThrows(OutOfBoardException.class, () -> {
+            move.promptNextTurn(1);
+            move.readMove(1);
+            move.makeMove(1);
+        });
     }
 
     @Test
     @Disabled
-    void displayTheCorrectNextTurnMessage() {
-        //move.PromptNextTurn(1);
-        String expectedOutput = "Black turn: enter your move (e.g. 3 4):" + System.lineSeparator();
-        //assertEquals(expectedOutput, outContent.toString());
-    }
-
-    @AfterEach
-    @Disabled
-    public void restoreStreams() {
-        //System.setOut(originalOut);
+    public void testGame_StartGame() {
+        assertDoesNotThrow(() -> game.startGame());
     }
 }
