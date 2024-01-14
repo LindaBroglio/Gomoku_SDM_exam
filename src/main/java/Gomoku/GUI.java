4c35017266cfaf695780d4a9609e4b80a2939944
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-14 15:28:26 +0100

    Improved GUI

--- src/main/java/Gomoku/GUI.java
+++ src/main/java/Gomoku/GUI.java
@@ -5,6 +5,7 @@ import Gomoku.Exceptions.InputExceptions.OutOfBoardException;
 import Gomoku.Exceptions.InputExceptions.TakenNodeException;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.geom.Line2D;
 
@@ -42,33 +43,55 @@ public class GUI {
                 setBackground(new Color(255, 204, 153));
                 Graphics2D g2d = (Graphics2D) g;
                 g2d.setColor(Color.BLACK);
-                for (int i = 0; i < size; i++) {
-                    g2d.draw(new Line2D.Float(cellSize * i, 0, cellSize * i, boardSize));
-                    g2d.draw(new Line2D.Float(0, cellSize * i, boardSize, cellSize * i));
+                for (int i = 0; i <= size + 1; i++) {
+                    g2d.draw(new Line2D.Float(cellSize * i, 0, cellSize * i, boardSize + cellSize));
+                    g2d.draw(new Line2D.Float(0, cellSize * i, boardSize + cellSize, cellSize * i));
                 }
             }
         };
-        boardPanel.setPreferredSize(new Dimension(boardSize, boardSize));
+        boardPanel.setPreferredSize(new Dimension(boardSize + cellSize + 1, boardSize + cellSize + 1));
         boardPanel.setLayout(null);
 
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                JButton button = new JButton();
+        class CircleButton extends JButton {
+            private Color circleColor;
+
+            public CircleButton() {
+                setOpaque(false);
+                setContentAreaFilled(false);
+                setBorderPainted(false);
+            }
+
+            public void setCircleColor(Color color) {
+                this.circleColor = color;
+                repaint();
+            }
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (circleColor != null) {
+                    g.setColor(circleColor);
+                    g.fillOval(0, 0, getWidth() - 1, getHeight() - 1);
+                }
+            }
+        }
+
+
+        for (int x = 0; x <= size + 1; x++) {
+            for (int y = 0; y <= size + 1; y++) {
+                CircleButton button = new CircleButton();
                 button.setBounds((x * cellSize) - (cellSize / 2), (y * cellSize) - (cellSize / 2), cellSize, cellSize);
-                button.setOpaque(false);
-                button.setContentAreaFilled(false);
-                button.setBorderPainted(false);
                 Integer[] coordinates = new Integer[]{x, y};
                 button.addActionListener(e -> {
                     try {
-                        button.setBackground(game.isBlackTurn() ? Color.BLACK : Color.WHITE);
-                        button.setContentAreaFilled(true);
+                        button.setCircleColor(game.isBlackTurn() ? Color.BLACK : Color.WHITE);
+                        button.setContentAreaFilled(false);
                         game.makeMove(coordinates);
                     } catch (TakenNodeException ex) {
-                        button.setBackground(game.isBlackTurn() ? Color.WHITE : Color.BLACK);
+                        button.setCircleColor(game.isBlackTurn() ? Color.WHITE : Color.BLACK);
                         JOptionPane.showMessageDialog(gridFrame, "This node is already taken. Please choose another one.", "Invalid Move", JOptionPane.ERROR_MESSAGE);
                     } catch (OutOfBoardException ex) {
-                        button.setContentAreaFilled(false);
+                        button.setCircleColor(null);
                         JOptionPane.showMessageDialog(gridFrame, "This move is out of the board boundaries. Please choose a valid position.", "Invalid Move", JOptionPane.ERROR_MESSAGE);
                     } catch (GameWonException ex) {
                         JOptionPane.showMessageDialog(gridFrame, game.isBlackTurn() ? "Black wins!" : "White wins!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
@@ -79,9 +102,14 @@ public class GUI {
             }
         }
 
-        gridFrame.add(boardPanel);
+        JPanel containerPanel = new JPanel(new GridBagLayout());
+        containerPanel.setBorder(new EmptyBorder(50, 50, 50, 50));
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.CENTER;
+        containerPanel.add(boardPanel, gbc);
+        containerPanel.setSize(new Dimension(boardSize, boardSize));
+        gridFrame.setContentPane(containerPanel);
         gridFrame.pack();
-        gridFrame.setSize(new Dimension(boardSize, boardSize));
         gridFrame.setLocationRelativeTo(null);
         gridFrame.setVisible(true);
     }
