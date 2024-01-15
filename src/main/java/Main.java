2868091ba8d0bed36431698c66572ca69c5eb7ee
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-15 17:16:26 +0100

    Refactoring according to SonarQube suggestions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to play Gomoku with a GUI or CLI? Enter 'GUI' or 'CLI':");
-        String interfaceType = scanner.nextLine().trim();
+        System.out.println("Hello! Do you want to play Gomoku with a GUI or CLI? Enter 'GUI' or 'CLI' (or 'quit' to exit):");
+        while (true) {
+            String interfaceType = scanner.nextLine().trim();
 
-        if ("GUI".equalsIgnoreCase(interfaceType)) {
-            SwingUtilities.invokeLater(GUI::new);
-        } else if ("CLI".equalsIgnoreCase(interfaceType)) {
-            CLI cli;
-            try {
-                cli = new CLI();
-                cli.playFromCommandLine();
-            } catch (QuitException e) {
-                System.out.println(e.getMessage());
+            if ("quit".equalsIgnoreCase(interfaceType)) {
+                System.out.println("You decided to exit the game before starting, ciao!");
                 System.exit(0);
+            } else if ("GUI".equalsIgnoreCase(interfaceType)) {
+                SwingUtilities.invokeLater(GUI::new);
+                break;
+            } else if ("CLI".equalsIgnoreCase(interfaceType)) {
+                CLI cli;
+                try {
+                    cli = new CLI();
+                    cli.playFromCommandLine();
+                } catch (QuitException e) {
+                    System.out.println(e.getMessage());
+                    System.exit(0);
+                }
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter 'GUI' or 'CLI' (or 'quit' to exit).");
             }
-        } else {
-            System.out.println("Invalid input. Please enter 'GUI' or 'CLI'.");
         }
     }
 }
