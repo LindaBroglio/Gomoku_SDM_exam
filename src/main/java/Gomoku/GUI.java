b66cd8cea533701c727d4aa5f2dec49c16048a9f
NicolaPerin <98810769+NicolaPerin@users.noreply.github.com>
2024-01-16 01:27:16 +0100

    Refactored GUI class to reduce cognitive complexity

--- src/main/java/Gomoku/GUI.java
+++ src/main/java/Gomoku/GUI.java
@@ -10,6 +10,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.geom.Line2D;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,49 +25,75 @@ public class GUI {
     public GUI() {
         size = chooseBoardSize();
         if (size == null) System.exit(0);
-        Integer[] gameSpecification = new Integer[]{size, 5};
-        startNewGame(gameSpecification);
+        startNewGame(new Integer[]{size, 5});
+        JFrame gridFrame = createGridFrame();
+        createButtons();
+        JButton resignButton = createResignButton();
+        JPanel mainPanel = createMainPanel(resignButton);
+        setBackgroundPanel(gridFrame, mainPanel);
+        gridFrame.pack();
+        gridFrame.setLocationRelativeTo(null);
+        gridFrame.setVisible(true);
+    }
+
+    private JFrame createGridFrame() {
         JFrame gridFrame = new JFrame("Gomoku");
         gridFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return gridFrame;
+    }
+
+    private void createButtons() {
         Integer cellSize = 550 / size;
         Integer boardSize = cellSize * size;
         boardPanel = createBoardPanel(cellSize, boardSize);
         messageLabel = createMessageLabel();
-
         for (int x = 0; x <= size + 1; x++) {
             for (int y = 0; y <= size + 1; y++) {
-                //ImageIcon blackIcon = new ImageIcon(getClass().getResource("/blackStone.png"));
-                //ImageIcon whiteIcon = new ImageIcon(getClass().getResource("/whiteStone.png"));
-                CircleButton button = new CircleButton();
-                button.setBounds((x * cellSize) - (cellSize / 2), (y * cellSize) - (cellSize / 2), cellSize, cellSize);
-                Integer[] coordinates = new Integer[]{x, y};
-                messageLabel.setText("Black turn!");
-                button.addActionListener(e -> {
-                    try {
-                        game.makeMove(coordinates);
-                        button.setCircleColor(game.isBlackTurn() ? Color.WHITE : Color.BLACK);
-                        //button.setCircleIcon(game.isBlackTurn() ? whiteIcon: blackIcon);
-                        if (!game.boardIsNotFull()) handleDraw();
-                        messageLabel.setText(game.isBlackTurn() ? "Black turn" : "White turn");
-                    } catch (TakenNodeException ex) {
-                        messageLabel.setText("This node is already taken, change spot!");
-                    } catch (OutOfBoardException ex) {
-                        button.setCircleColor(null);
-                        //button.setIcon(null);
-                        messageLabel.setText("You tried a move out of the board: change spot!");
-                    } catch (GameWonException ex) {
-                        button.setCircleColor(game.isBlackTurn() ? Color.BLACK : Color.WHITE);
-                        //button.setCircleIcon(game.isBlackTurn() ? blackIcon: whiteIcon);
-                        messageLabel.setText(game.isBlackTurn() ? "Black wins!" : "White wins!");
-                        handleGameWon();
-                    }
-                });
-                boardPanel.add(button);
+                boardPanel.add(createButton(new Integer[]{x, y}, cellSize));
             }
         }
+    }
 
-        JButton resignButton = getResignButton();
+    private CircleButton createButton(Integer[] coordinates, int cellSize) {
+        ImageIcon blackIcon = new ImageIcon(getClass().getResource("/blackStone.png"));
+        ImageIcon whiteIcon = new ImageIcon(getClass().getResource("/whiteStone.png"));
+        CircleButton button = new CircleButton();
+        button.setBounds((coordinates[0] * cellSize) - (cellSize / 2), (coordinates[1] * cellSize) - (cellSize / 2), cellSize, cellSize);
+        messageLabel.setText("Black turn!");
+        button.addActionListener(e -> handleButtonAction(button, coordinates, blackIcon, whiteIcon));
+        return button;
+    }
 
+    private void handleButtonAction(CircleButton button, Integer[] coordinates, ImageIcon blackIcon, ImageIcon whiteIcon) {
+        try {
+            game.makeMove(coordinates);
+            button.setCircleIcon(game.isBlackTurn() ? whiteIcon : blackIcon);
+            if (!game.boardIsNotFull()) handleDraw();
+            messageLabel.setText(game.isBlackTurn() ? "Black turn" : "White turn");
+        } catch (TakenNodeException ex) {
+            handleTakenNodeException();
+        } catch (OutOfBoardException ex) {
+            handleOutOfBoardException(button);
+        } catch (GameWonException ex) {
+            handleGameWonException(button, blackIcon, whiteIcon);
+        }
+    }
+
+    private void handleTakenNodeException() { messageLabel.setText("This node is already taken, change spot!"); }
+
+    private void handleOutOfBoardException(CircleButton button) {
+        button.setIcon(null);
+        messageLabel.setText("You tried a move out of the board: change spot!");
+    }
+
+    private void handleGameWonException(CircleButton button, ImageIcon blackIcon, ImageIcon whiteIcon) {
+        button.setCircleIcon(game.isBlackTurn() ? blackIcon : whiteIcon);
+        messageLabel.setText(game.isBlackTurn() ? "Black wins!" : "White wins!");
+        handleGameWon();
+    }
+
+
+    private JPanel createMainPanel(JButton resignButton) {
         JPanel boardContainerPanel = new JPanel(new BorderLayout());
         boardContainerPanel.add(boardPanel, BorderLayout.CENTER);
 
@@ -74,41 +101,31 @@ public class GUI {
         resignButtonPanel.setOpaque(false);
         resignButtonPanel.add(resignButton);
 
-        JButton quitButton = new JButton("Quit");
-        quitButton.setBounds(10, boardSize + cellSize + 10, 80, 30);
+        JPanel messagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        messagePanel.setOpaque(false);
+        messagePanel.add(messageLabel);
 
-        quitButton.addActionListener(e -> {
-            int option = JOptionPane.showOptionDialog(
-                    boardPanel,
-                    "Do you want to quit the game?",
-                    "Quit Game",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    null,
-                    null);
-            if (option == JOptionPane.YES_OPTION) handleResignation();
-        });
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBorder(new EmptyBorder(50, 60, 40, 60));
+        mainPanel.setOpaque(false);
 
-        boardContainerPanel.add(boardPanel, BorderLayout.CENTER);
+        mainPanel.add(messagePanel);
+        mainPanel.add(boardContainerPanel);
+        mainPanel.add(Box.createVerticalStrut(25));
+        mainPanel.add(resignButtonPanel, BorderLayout.SOUTH);
 
-        JPanel quitButtonPanel = new JPanel();
-        quitButtonPanel.add(quitButton);
-
-        JPanel containerPanel = new JPanel(new BorderLayout());
-        containerPanel.setBorder(new EmptyBorder(50, 50, 50, 50));
-        containerPanel.add(messageLabel, BorderLayout.NORTH);
-        containerPanel.add(boardContainerPanel, BorderLayout.CENTER);
-        containerPanel.add(quitButtonPanel, BorderLayout.SOUTH);
-
-        gridFrame.setContentPane(containerPanel);
-        gridFrame.setContentPane(containerPanel);
-        gridFrame.pack();
-        gridFrame.setLocationRelativeTo(null);
-        gridFrame.setVisible(true);
+        return mainPanel;
     }
 
-    private JButton getResignButton() {
+    private void setBackgroundPanel(JFrame gridFrame, JPanel mainPanel) {
+        BackgroundPanel backgroundPanel = new BackgroundPanel("/bg.jpg");
+        backgroundPanel.setLayout(new BorderLayout());
+        backgroundPanel.add(mainPanel, BorderLayout.CENTER);
+        gridFrame.setContentPane(backgroundPanel);
+    }
+
+    private JButton createResignButton() {
         JButton resignButton = new JButton();
         resignButton.setPreferredSize(new Dimension(90, 30));
         resignButton.setText("Resign");
@@ -119,25 +136,27 @@ public class GUI {
         resignButton.setOpaque(false);
 
         resignButton.addActionListener(e -> {
-            int option = JOptionPane.showOptionDialog(
-                    boardPanel,
-                    "Do you want to quit the game?",
-                    "Quit Game",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    null,
-                    null);
+            int option = JOptionPane.showOptionDialog(boardPanel,
+                "Do you want to quit the game?",
+                "Quit Game",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                null,
+                null);
             if (option == JOptionPane.YES_OPTION) handleResignation();
         });
         return resignButton;
     }
 
     private void handleGameWon() {
-        int option = JOptionPane.showOptionDialog(boardPanel,"Gomoku! " + (game.isBlackTurn() ? "Black" : "White") + " wins!\nDo you want to start a new game?",
+        int option = JOptionPane.showOptionDialog(boardPanel,
+                "Gomoku! " + (game.isBlackTurn() ? "Black" : "White") +
+                        " wins!" + System.lineSeparator() +
+                        "Do you want to start a new game?",
                 "GOMOKU!", JOptionPane.YES_NO_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,null,
-                null,null);
+                JOptionPane.INFORMATION_MESSAGE,
+                null, null,null);
         if (option == JOptionPane.YES_OPTION) restartGame();
         else System.exit(0);
     }
@@ -148,36 +167,12 @@ public class GUI {
         SwingUtilities.invokeLater(GUI::new);
     }
 
-    class CircleButton extends JButton {
-        private Color circleColor;
-
-        public CircleButton() {
-            setOpaque(false);
-            setContentAreaFilled(false);
-            setBorderPainted(false);
-        }
-
-        public void setCircleColor(Color color) {
-            this.circleColor = color;
-            repaint();
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (circleColor != null) {
-                g.setColor(circleColor);
-                g.fillOval(0, 0, getWidth() - 1, getHeight() - 1);
-            }
-        }
-    }
-
     private JLabel createMessageLabel() {
         messageLabel = new JLabel();
         messageLabel.setForeground(Color.PINK);
         Font labelFont = new Font("Courier", Font.PLAIN, 17);
         messageLabel.setFont(labelFont);
-        messageLabel.setHorizontalAlignment(JLabel.CENTER);
+        messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         messageLabel.setOpaque(false);
         messageLabel.setBorder(new EmptyBorder(0, 0, 20, 0));
         return messageLabel;
@@ -254,5 +249,44 @@ public class GUI {
                 null);
         if (option == JOptionPane.YES_OPTION) restartGame();
         else System.exit(0);
+    }
+
+    class CircleButton extends JButton {
+        private ImageIcon circleIcon;
+
+        public CircleButton() {
+            setOpaque(false);
+            setContentAreaFilled(false);
+            setBorderPainted(false);
+        }
+
+        public void setCircleIcon(ImageIcon icon) {
+            this.circleIcon = icon;
+            repaint();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (circleIcon != null) {
+                g.drawImage(circleIcon.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        }
+    }
+
+    static class BackgroundPanel extends JPanel {
+        private Image backgroundImage;
+
+        public BackgroundPanel(String imagePath) {
+            URL imageUrl = getClass().getResource(imagePath);
+            if (imageUrl != null) backgroundImage = new ImageIcon(imageUrl).getImage();
+            else System.err.println("Resource not found: " + imagePath);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
